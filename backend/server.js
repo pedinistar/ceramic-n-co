7c10dcd4430dc3f34a5cff63c30222dfc867e478
jayashreePedini <jayashree14work@gmail.com>
2024-12-11 12:40:07 +0530

    Create Backend Folder File Structure

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+require("dotenv").config(); // Load environment variables
+const mongoose = require("mongoose");
+
+// Connect to MongoDB
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("Connected to MongoDB Atlas"))
+  .catch((err) => console.error("Error connecting to MongoDB:", err));
